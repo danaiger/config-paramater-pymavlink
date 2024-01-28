6885a697b88327f5a69c27a8dc30ba58c2f19b44
dan <dan@dan>
2024-01-28 15:34:25 +0200

    add timeout error in heartbeat

--- config_parameter.py
+++ config_parameter.py
@@ -24,7 +24,9 @@ def set_a_parameter_at_on_board_computer(parameter_name:str,parameter_value:int,
 
 def main():
     connection:mavfile = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
-    connection.wait_heartbeat(timeout=TIMEOUT_SECOND)
+    heartbit=connection.wait_heartbeat(timeout=TIMEOUT_SECOND)
+    if heartbit is None:
+        raise TimeoutError("cannot get heartbeat, please check the connection")
     set_a_parameter_at_on_board_computer(PARAMETER_NAME,GPS_AUTO_SWITCH.USE_BEST.value,connection,TIMEOUT_SECOND)
 
 if __name__=='__main__':
