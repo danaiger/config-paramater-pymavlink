c745e098fc5e419eaca0a93d18c95d9f58106654
dan <dan@dan>
2024-01-28 15:07:13 +0200

    add enum

--- config_parameter.py
+++ config_parameter.py
@@ -1,5 +1,12 @@
 from pymavlink import mavutil
 from pymavlink.mavutil import mavfile
+from enum import Enum
+
+class GPS_AUTO_SWITCH(Enum):
+    USE_PRIMARY=0
+    USE_BEST=1
+    BLEND=2
+    USE_PRIMARY_IF_3D_FIX_OR_BETTER=4
 
 TIMEOUT_SECOND=3
 PARAMETER_NAME="GPS_AUTO_SWITCH"
@@ -7,14 +14,14 @@ PARAMETER_NAME="GPS_AUTO_SWITCH"
 connection:mavfile = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
 connection.wait_heartbeat(timeout=TIMEOUT_SECOND)
 
-def set_a_parameter_at_on_board_computer(parameter_name:str,sock:mavfile,timeout:int=TIMEOUT_SECOND):
+def set_a_parameter_at_on_board_computer(parameter_name:str,sock:mavfile,timeout_seconds:int=TIMEOUT_SECOND):
     sock.mav.param_set_send(
         sock.target_system, sock.target_component,
-        bytes(PARAMETER_NAME,'utf-8'),
+        bytes(parameter_name,'utf-8'),
         2,
         mavutil.mavlink.MAV_PARAM_TYPE_REAL32
     )
-    message = sock.recv_match(type='PARAM_VALUE', blocking=True,timeout=3).to_dict()
+    message = sock.recv_match(type='PARAM_VALUE', blocking=True,timeout=timeout_seconds).to_dict()
     print('name: %s\tvalue: %d' %
           (message['param_id'], message['param_value']))
 
