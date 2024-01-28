46b3a442c6eebfa16172711dd129ca52595422d8
dan <dan@dan>
2024-01-28 16:02:00 +0200

    WIP

--- config_parameter.py
+++ config_parameter.py
@@ -1,3 +1,4 @@
+import time
 from pymavlink import mavutil
 from pymavlink.mavutil import mavfile
 from enum import Enum
@@ -8,24 +9,34 @@ class GPS_AUTO_SWITCH(Enum):
     BLEND=2
     USE_PRIMARY_IF_3D_FIX_OR_BETTER=4
 
-TIMEOUT_SECOND=3
-PARAMETER_NAME="GPS_AUTO_SWITCH"
 
-def set_a_parameter_at_on_board_computer(parameter_name:str,parameter_value:int,sock:mavfile,timeout_seconds:int=TIMEOUT_SECOND):
+def set_a_parameter_at_on_board_computer(parameter_name:str,parameter_value:int,sock:mavfile,timeout_seconds:int):
     sock.mav.param_set_send(
         sock.target_system, sock.target_component,
         bytes(parameter_name,'utf-8'),
         parameter_value,
         mavutil.mavlink.MAV_PARAM_TYPE_REAL32
     )
-    message = sock.recv_match(type='PARAM_VALUE', blocking=True,timeout=timeout_seconds).to_dict()
-    print('name: %s\tvalue: %d' %
-          (message['param_id'], message['param_value']))
+    print(f"request sent to configure param name: {parameter_name} with value: {parameter_value}. waiting for acknowledgement")
+    now=time.time()
+    while True:
+        if time.time()>now+timeout_seconds:
+            raise TimeoutError("something went wrong, please try again")
+        message = sock.recv_match(type='PARAM_VALUE', blocking=True,timeout=timeout_seconds)
+        if not message:
+            raise TimeoutError("something went wrong, please try again")
+        parsed_message=message.to_dict()
+        if parsed_message.get(parameter_name) != parameter_value:
+            continue
+        print('parameter name: %s\tvalue: %d' %
+              (parsed_message['param_id'], parsed_message['param_value']))
 
 def main():
+    TIMEOUT_SECOND=3
+    PARAMETER_NAME="GPS_AUTO_SWITCH"
     connection:mavfile = mavutil.mavlink_connection('udpin:0.0.0.0:14550')
-    heartbit=connection.wait_heartbeat(timeout=TIMEOUT_SECOND)
-    if heartbit is None:
+    heartbeat=connection.wait_heartbeat(timeout=TIMEOUT_SECOND)
+    if not heartbeat:
         raise TimeoutError("cannot get heartbeat, please check the connection")
     set_a_parameter_at_on_board_computer(PARAMETER_NAME,GPS_AUTO_SWITCH.USE_BEST.value,connection,TIMEOUT_SECOND)
 
