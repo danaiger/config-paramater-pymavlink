752e07a86f36ce601ceffc96e7643fae81d40dbf
dan <dan@dan>
2024-01-30 00:01:39 +0200

    added keywords

--- test_config_parameter.py
+++ test_config_parameter.py
@@ -15,15 +15,16 @@ from pymavlink import mavutil
 from pymavlink.dialects.v20.ardupilotmega import MAVLink_param_value_message
 import time
 
+
 @pytest.fixture
 def gps_auto_switch_mavlink_message_example():
     return MAVLink_param_value_message(
-    param_id= b"GPS_AUTO_SWITCH",
-    param_value= 4.0,
-    param_type= 2,
-    param_count= 1386,
-    param_index= 65535,
-)
+        param_id=b"GPS_AUTO_SWITCH",
+        param_value=4.0,
+        param_type=2,
+        param_count=1386,
+        param_index=65535,
+    )
 
 
 @pytest.fixture
@@ -36,8 +37,11 @@ def test_wait_for_heartbeat_raises_timeout_error(connection):
     with pytest.raises(TimeoutError):
         wait_for_heartbeat(connection, timeout_seconds=3)
 
+
 def test_wait_for_heartbeat_not_raises(connection):
-    connection.wait_heartbeat = Mock(return_value="heartbeat string since I dont know how to generate heatbeat")
+    connection.wait_heartbeat = Mock(
+        return_value="heartbeat string since I dont know how to generate heatbeat"
+    )
     wait_for_heartbeat(connection, timeout_seconds=3)
 
 
@@ -46,25 +50,33 @@ def test_assert_not_exceeding_timeout_limit_raises_when_exceeding_limit():
         assert_not_exceeding_timeout_limit(time_now=time.time() - 4, timeout_seconds=3)
 
 
-def test_is_received_message_is_the_relevant_ack_returns_true_for_correct_ack(gps_auto_switch_mavlink_message_example):
+def test_is_received_message_is_the_relevant_ack_returns_true_for_correct_ack(
+    gps_auto_switch_mavlink_message_example,
+):
     assert _is_received_message_is_the_relevant_ack(
         gps_auto_switch_mavlink_message_example.to_dict(), "GPS_AUTO_SWITCH", 4
     )
 
 
-def test_is_received_message_is_the_relevant_ack_returns_false_for_incorrect_value_ack(gps_auto_switch_mavlink_message_example):
+def test_is_received_message_is_the_relevant_ack_returns_false_for_incorrect_value_ack(
+    gps_auto_switch_mavlink_message_example,
+):
     assert not _is_received_message_is_the_relevant_ack(
         gps_auto_switch_mavlink_message_example.to_dict(), "GPS_AUTO_SWITCH", 3
     )
 
 
-def test_is_received_message_is_the_relevant_ack_returns_false_for_incorrect_param_ack(gps_auto_switch_mavlink_message_example):
+def test_is_received_message_is_the_relevant_ack_returns_false_for_incorrect_param_ack(
+    gps_auto_switch_mavlink_message_example,
+):
     assert not _is_received_message_is_the_relevant_ack(
         gps_auto_switch_mavlink_message_example.to_dict(), "GPS_AUTO_CONFIG", 1
     )
 
 
-def test_get_next_message_of_type_parameter_value_returns_correct_message(connection,gps_auto_switch_mavlink_message_example):
+def test_get_next_message_of_type_parameter_value_returns_correct_message(
+    connection, gps_auto_switch_mavlink_message_example
+):
     connection.recv_match = Mock(return_value=gps_auto_switch_mavlink_message_example)
     assert (
         _get_next_message_of_type_parameter_value(connection, 3)
@@ -81,12 +93,15 @@ def test_get_next_message_of_type_parameter_value_raises_when_no_message_is_rece
 
 
 def test_wait_for_ack_that_parameter_has_been_configured_successfuly_returns_correct_parsed_message(
-    connection,gps_auto_switch_mavlink_message_example
+    connection, gps_auto_switch_mavlink_message_example
 ):
     connection.recv_match = Mock(return_value=gps_auto_switch_mavlink_message_example)
     assert (
         wait_for_ack_that_parameter_has_been_configured_successfuly(
-            "GPS_AUTO_SWITCH", 4, connection, 3
+            parameter_name="GPS_AUTO_SWITCH",
+            expected_parameter_value=4,
+            sock=connection,
+            timeout_seconds=3,
         )
         == gps_auto_switch_mavlink_message_example.to_dict()
     )
