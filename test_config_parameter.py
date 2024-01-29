f89eb142ca6115870834da0da29c0c1c6af8e02d
dan <dan@dan>
2024-01-29 21:15:49 +0200

    added test for is_received_message_is_the_relevant_ack

--- test_config_parameter.py
+++ test_config_parameter.py
@@ -1,8 +1,21 @@
 import pytest
-from config_parameter import wait_for_heartbeat, _assert_not_exceeding_timeout_limit
+from config_parameter import (
+    wait_for_heartbeat,
+    _assert_not_exceeding_timeout_limit,
+    _is_received_message_is_the_relevant_ack,
+)
 from unittest.mock import Mock
 from pymavlink import mavutil
 import time
+
+GPS_AUTO_SWITCH_MESSAGE_EXAMPLE = {
+    "mavpackettype": "PARAM_VALUE",
+    "param_id": "GPS_AUTO_SWITCH",
+    "param_value": 4.0,
+    "param_type": 2,
+    "param_count": 1386,
+    "param_index": 65535,
+}
 
 
 @pytest.fixture
@@ -18,4 +31,21 @@ def test_heartbeat_raises_timeout_error(connection):
 
 def test_assert_exceeding_timeout_limits_raises():
     with pytest.raises(TimeoutError):
-        _assert_not_exceeding_timeout_limit(time_now=time.time()-4,timeout_seconds=3)
+        _assert_not_exceeding_timeout_limit(time_now=time.time() - 4, timeout_seconds=3)
+
+
+def test_is_received_message_is_the_relevant_ack_returns_true_for_correct_ack():
+    assert _is_received_message_is_the_relevant_ack(
+        GPS_AUTO_SWITCH_MESSAGE_EXAMPLE, "GPS_AUTO_SWITCH", 4
+    )
+
+
+def test_is_received_message_is_the_relevant_ack_returns_false_for_incorrect_value_ack():
+    assert not _is_received_message_is_the_relevant_ack(
+        GPS_AUTO_SWITCH_MESSAGE_EXAMPLE, "GPS_AUTO_SWITCH", 3
+    )
+
+def test_is_received_message_is_the_relevant_ack_returns_false_for_incorrect_param_ack():
+    assert not _is_received_message_is_the_relevant_ack(
+        GPS_AUTO_SWITCH_MESSAGE_EXAMPLE, "GPS_AUTO_CONFIG", 1
+    )
